fa9a724e52f595aa55ae748284bb9fddc96992a3
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-02-17 10:35:13 -0500

    ajout du footer

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  
 }
+
+
+function add_google_fonts() {
+    wp_enqueue_style( 'google_web_fonts', 'https://fonts.googleapis.com/css?family=Roboto&display=swap' );
+  }
+  add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
